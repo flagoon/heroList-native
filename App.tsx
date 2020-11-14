f5d5d6c89e2ce64451406d79afc43df64f32c0af
Pawel -Muody- Kochanek <pawel.kochanek@gmail.com>
2020-11-14 19:54:48 +0100

    Change safe are view for better component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomePage from 'pages/WelcomePage';
